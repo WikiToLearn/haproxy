9f0ee5e82ad28eeb8c4baab775b6bda330f28b99
Toma Luca <toma.luca95@gmail.com>
2015-11-06 09:44:07 +0100

    Fixed sslv3 and ciphersuite issues

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-FROM haproxy:1.5
+FROM haproxy:1.6
 MAINTAINER wikitolearn sysadmin@wikitolearn.org
-RUN apt-get update
-RUN apt-get install openssl --assume-yes && rm -f /var/cache/apt/archives/*deb
-RUN apt-get install wget --assume-yes && rm -f /var/cache/apt/archives/*deb 
+RUN apt-get update && apt-get install openssl --assume-yes && rm -f /var/cache/apt/archives/*deb && find /var/lib/apt/lists/ -type f -delete
+RUN apt-get update && apt-get install wget --assume-yes && rm -f /var/cache/apt/archives/*deb && find /var/lib/apt/lists/ -type f -delete
 ADD ./cloudflare.txt /
 ADD ./haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
 ADD ./kickstart.sh /
--- kickstart.sh
+++ kickstart.sh
@@ -10,8 +10,8 @@ else
  openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
  openssl rsa -passin pass:x -in server.pass.key -out /etc/ssl/private/haproxy.key
  rm server.pass.key
- openssl req -new -key /etc/ssl/private/haproxy.key -out server.csr -subj "/C=IT/ST=Italia/L=Milano/O=WikiToLearn/OU=HAproxy IT Department/CN=www.wikitolearn.org"
- openssl x509 -req -days 365000 -in server.csr -signkey /etc/ssl/private/haproxy.key -out /etc/ssl/certs/haproxy.crt
+ openssl req -sha256 -new -key /etc/ssl/private/haproxy.key -out server.csr -subj "/C=IT/ST=Italia/L=Milano/O=WikiToLearn/OU=HAproxy IT Department/CN=www.wikitolearn.org"
+ openssl x509 -req -sha256 -days 365000 -in server.csr -signkey /etc/ssl/private/haproxy.key -out /etc/ssl/certs/haproxy.crt
  rm server.csr
 
  cat /etc/ssl/certs/haproxy.crt /etc/ssl/private/haproxy.key > /haproxy.pem
