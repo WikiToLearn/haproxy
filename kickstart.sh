aca5e306d6ac9a26fec6e4af815d5d55359df312
Toma Luca <toma.luca95@gmail.com>
2016-03-25 04:00:00 +0100

    modifyed kickstart script to allow certs

--- kickstart.sh
+++ kickstart.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-if [[ -d /certs/ ]] ; then
- cd /certs/
- cat websrv.crt websrv.key > /haproxy.pem
+if [[ -f /certs/websrv.crt ]] ; then
+    if [[ -f /certs/websrv.key ]] ; then
+        cd /certs/
+        cat websrv.crt websrv.key > /haproxy.pem
+    fi
 else
- rm -f /etc/ssl/private/haproxy.key
- rm -f /etc/ssl/certs/haproxy.crt
+    if [[ ! -f /etc/ssl/private/haproxy.key ]] || [[ ! -f /etc/ssl/certs/haproxy.crt ]] ; then
+        rm -f /etc/ssl/private/haproxy.key
+        rm -f /etc/ssl/certs/haproxy.crt
 
- openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
- openssl rsa -passin pass:x -in server.pass.key -out /etc/ssl/private/haproxy.key
- rm server.pass.key
- openssl req -sha256 -new -key /etc/ssl/private/haproxy.key -out server.csr -subj "/C=IT/ST=Italia/L=Milano/O=WikiToLearn/OU=HAproxy IT Department/CN=www.wikitolearn.org"
- openssl x509 -req -sha256 -days 365000 -in server.csr -signkey /etc/ssl/private/haproxy.key -out /etc/ssl/certs/haproxy.crt
- rm server.csr
+        openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
+        openssl rsa -passin pass:x -in server.pass.key -out /etc/ssl/private/haproxy.key
+        rm server.pass.key
+        openssl req -sha256 -new -key /etc/ssl/private/haproxy.key -out server.csr -subj "/C=IT/ST=Italia/L=Milano/O=WikiToLearn/OU=HAproxy IT Department/CN=www.wikitolearn.org"
+        openssl x509 -req -sha256 -days 365000 -in server.csr -signkey /etc/ssl/private/haproxy.key -out /etc/ssl/certs/haproxy.crt
+        rm server.csr
+    fi
 
- cat /etc/ssl/certs/haproxy.crt /etc/ssl/private/haproxy.key > /haproxy.pem
+    cat /etc/ssl/certs/haproxy.crt /etc/ssl/private/haproxy.key > /haproxy.pem
 fi
 
 exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
--- localbuild.sh
+++ localbuild.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t wikitolearn/haproxy:0.6.1 .
+docker build -t wikitolearn/haproxy:0.6.2 .
