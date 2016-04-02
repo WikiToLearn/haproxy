c5fe512af512ae4b2b3e0373c77dab8d0a53204c
Toma Luca <toma.luca95@gmail.com>
2016-04-02 03:01:03 +0200

    Fixed subnet range

--- update-x-forwarded-for-acl-file.sh
+++ update-x-forwarded-for-acl-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm x-forwarded-for-acl
 wget -O x-forwarded-for-acl https://www.cloudflare.com/ips-v4
-echo "192.168.0.0/24" >> x-forwarded-for-acl
+echo "192.168.0.0/16" >> x-forwarded-for-acl
 echo "172.16.0.0/12"  >> x-forwarded-for-acl
 echo "10.0.0.0/8"     >> x-forwarded-for-acl
